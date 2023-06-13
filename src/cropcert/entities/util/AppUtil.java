0c6532ed31e2e4783b74973b3d23b415880afa94
arunasd463 <arunbala.balasubramani@strandls.com>
2023-06-13 19:04:25 +0530

    refactor: :recycle: pr sugestion updates

--- src/cropcert/entities/util/AppUtil.java
+++ src/cropcert/entities/util/AppUtil.java
@@ -1,18 +1,18 @@
 package cropcert.entities.util;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.EnumMap;
 
 public class AppUtil {
 
-	protected static final Map<MODULE, List<String>> ALLOWED_CONTENT_TYPES = new EnumMap<>(MODULE.class);
+	public static final Map<MODULE, List<String>> ALLOWED_CONTENT_TYPES = new HashMap<>();
 
 	static {
 		ALLOWED_CONTENT_TYPES.put(MODULE.ROLES, Arrays.asList("INSPECTOR", "ICS_MANAGER", "FARMER",
 				"COLLECTION_CENTER_PERSON", "COOPERATIVE_PERSON", "FACTORY_PERSON", "UNION_PERSON", "ADMIN"));
-	}
+	};
 
 	public enum MODULE {
 		ROLES, INSPECTOR, ICS_MANAGER, FARMER, COLLECTION_CENTER_PERSON, COOPERATIVE_PERSON, FACTORY_PERSON,
